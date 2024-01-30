ddfac7a0f712e3ad3c489ee489f0be674854ada1
dam23-23 <dam23-23@c16x18.iescierva.net>
2024-01-30 09:41:55 +0100

    Código que muestra mi nombre

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,5 +1,5 @@
 public class Ejemplo1{
-    public static void main(Strin[] args){
+    public static void main(String[] args){
             System.out.println("Holamundo");    
             String nombre = "David";
     System.out.println("Mi nombre es "+nombre);
